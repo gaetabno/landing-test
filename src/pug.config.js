60aff8ab9b810faaf686a20765ef647fcdfa3f13
Gaetano Bonaccio <gaetano.bonaccio@playground.it>
2023-07-01 13:42:25 +0200

    product, xplr section + initial mob

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	"locals": {
 		"translations":{
-			"shopnow":"Shop now"
+			"shopnow":"Shop now",
+			"shopmen":"Shop men",
+			"shopwomen":"Shop women"
 		},
 		"navbar": {
 			"back": "Back"
@@ -47,18 +49,54 @@ module.exports = {
 			"bg_image":"./assets/images/products-section-bg.png",
 			"copy1":"Through his custom graphics and eye-catching colours, this <span class='o-highlighted products-highlighted--green'>limited&#8209;edition</span> collection mirrors the parallels between the urban and natural worlds.",
 			"copy2":"Nature works on so many different scales. <span class='o-highlighted products-highlighted--purple'>It’s humbling</span>. <br>You realise you’re not at the centre of everything; you’re just a small part of a huge environment with lots of things going on in it.",		
-			"list":[
-				{
+			"list":{
+				"col1":{
 					"image1":"./assets/images/product-1.png",
-					"image2":"./assets/images/product-2.png",
-					"link":"#"
+					"image2":"./assets/images/product-3.png",
+					"link1":"#",
+					"link2":"#"
 				},
-				{
-					"image1":"./assets/images/product-3.png",
-					"image2":"./assets/images/product-4.png",
-					"link":"#"
+				"col2":{
+					"image1":"./assets/images/product-2.png",
+					"image2":"./assets/images/product-4.png"
 				}			
-			]
+			}
+			
+		},
+		"xplr_section":{
+			"images":["./assets/images/xplr-secion-image.png","./assets/images/xplr-secion-men.png"],
+			"icon":"./assets/images/svg/xplr-section-logo.svg",
+			"title":"Get a special gift with the NSE | Alfie Kungu collection",
+			"body":"Shop the drop and earn a sneak peek into Alfie Kungu's colourful world with an exclusive member-only stereoscope gift. <br><br>If you are already a member just buy from the NSE | Alfie Kungu collection <br><br>Be quick - gifts are limited!",
+			"tec":"terms and conditions apply",
+			"ctas":[
+				{
+					"label":"Become a member",
+					"link":"#"
+				},				
+				{
+					"label":"Sign in now",
+					"link":"#"
+				}
+			],
+			"white_section":{
+				"title":"HOW IT WORKS",
+				"steps":[
+					{
+						"link":"#",
+						"icon":"./assets/images/svg/xplr-icon.svg",
+						"copy":"<strong>1. <span class='underlined'>Sign up</span></strong> if you are not a XPLR Pass member yet	"					
+					},					{
+						"link":"#",
+						"icon":"./assets/images/svg/jacket-icon.svg",
+						"copy":"<strong>2.</strong> Add any item from the <strong class='underlined'>NSE | Alfie Kungu collection</strong> to your cart"					
+					},					{
+						"link":"#",
+						"icon":"./assets/images/svg/gift-icon.svg",
+						"copy":"<strong>3.</strong> Get a gift that will be shipped separately"					
+					}
+				]
+			}
 		}
 	}
 }
--- src/script.js
+++ src/script.js
@@ -13,6 +13,23 @@ hamb.addEventListener("click", (e) => {
     navbarDrawer.classList.toggle("open");
 });
 
+//---------------------
+
+let prev = 200;
+const navbar = document.querySelector(".js-navbar");
+const nseLogo = document.querySelector(".js-navbar .js-navbar__logo");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || 0;
+
+  if (scrollTop > prev) {
+    navbar.classList.add("visible"); 
+  } else {
+    navbar.classList.remove("visible");
+  }
+ 
+});
+
 //HOME
 
 const muteButton = document.querySelector(".js-mute__button");
@@ -65,16 +82,19 @@ function textScrolling() {
                     duration: 1,
                     delay: 0.5,
                 }
-            ).fromTo(cta, 
-                { 
-                    opacity: 0, 
+            ).fromTo(
+                cta,
+                {
+                    opacity: 0,
                 },
                 {
-                opacity: 1,
-                duration: 1,
-                duration: 1,
-                x: 200,
-            },"-=0.5");
+                    opacity: 1,
+                    duration: 1,
+                    duration: 1,
+                    x: 200,
+                },
+                "-=0.5"
+            );
         };
         gsap.fromTo(
             w,
@@ -97,22 +117,23 @@ function textScrolling() {
 }
 textScrolling();
 
-// // AUTHOR SLIDER
+// PRODUCT IMAGES ANIMATION
 
-// const authorSlider = () => {
-//     const section = document.getElementsByClassName('l-author');
-//     const copySection = document.getElementsByClassName('l-author__copy');
+const targets = document.querySelectorAll(".js-products__image");
 
-//     gsap.to(section,{
-//         scrollTrigger:{
-//             //markers: true,
-//             pin:copySection,
-//             trigger:section,
-//             start: "-=78",
-//             end: "bottom bottom",
-//             snap:1,
-//             duration:0.1
-//            }
-//     })
-// }
-// authorSlider();
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    },
+    { 
+    threshold: [0.8]
+    }
+);
+
+targets.forEach((image) => {
+    observer.observe(image);
+});
